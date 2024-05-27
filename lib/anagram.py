e2d261457f1919cd3fbc2a2e49ea5b68aefd4844
Tabithaokoth <kobbitho@gmail.com>
2024-05-27 20:12:53 +0300

    adds lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+class Anagram: #Anagram class to find words that are anagrams of a given word.
+
+  def __init__(self, word): #initialize the anagram with a given word
+    self.letters = sorted(list(word.lower()))  
+
+  def match(self, words):
+    matches = []
+    for word in words:
+      if sorted(list(word.lower())) == self.letters:  
+        matches.append(word)
+    return matches
+
+
